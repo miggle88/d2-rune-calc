b4fa0edb25a2e313320cf5479ffcf1787f5854a3
Mike Rogers <mtr293@gmail.com>
2022-10-20 00:31:43 -0400

    persist the inventory

--- src/server/routers/inventory.ts
+++ src/server/routers/inventory.ts
@@ -29,6 +29,39 @@ export const inventoryRouter = t.router({
     .use(authenticate)
     .input(z.object({}).catchall(z.number().min(0).max(999)))
     .mutation(async ({ ctx, input }) => {
+      for (const [name, count] of Object.entries(input)) {
+        // Find the existing rune inventory for the user/rune combo
+        const existingRune = await ctx.prisma.runeInventory.findFirst({
+          where: {
+            userId: ctx.session.userId,
+            rune: name,
+          },
+        })
+
+        if (existingRune) {
+          // If exists and the new count > 0, update
+          if (count > 0) {
+            await ctx.prisma.runeInventory.update({
+              data: { count },
+              where: { id: existingRune.id },
+            })
+          } else {
+            // If new count <= 0, delete the record
+            await ctx.prisma.runeInventory.delete({
+              where: { id: existingRune.id },
+            })
+          }
+        } else if (count > 0) {
+          // Create new record with the count when > 0
+          await ctx.prisma.runeInventory.create({
+            data: {
+              userId: ctx.session.userId,
+              rune: name,
+              count,
+            },
+          })
+        }
+      }
       return { message: 'ok' }
     }),
 })
